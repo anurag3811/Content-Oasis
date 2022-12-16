db916210ca424becb6c593162f34e10a56da9d2e
Anurag Singh <90863914+anurag3811@users.noreply.github.com>
2022-12-16 16:41:14 +0530

    fifth

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <title>Content Oasis</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
